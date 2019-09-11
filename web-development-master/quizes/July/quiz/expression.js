5858a3b1d5149b5a20d32c7e9c5de8b89bce235b
hadeelfleifel <hadeelfleifel@gmail.com>
2019-09-11 09:19:42 +0300

    drupal

--- web-development-master/quizes/July/quiz/expression.js
+++
@@ -1,8 +0,0 @@
-/*
- * Programming Quiz: First Expression 
- *
- * Write an expression that uses at least three, different, arithmetic operators
- * to log the number 42 to the console.
- */
-
-// this expression equals 4, change it to equal 42
